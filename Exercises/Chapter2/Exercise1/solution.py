e6c6dd203ff91b83cffee34e2d3f4c282e7c4767
Adam Carter <adam.clayton.carter@gmail.com>
2020-12-14 19:12:54 -0500

    Modify solution for Exercise 1 from Chapter 2

--- Exercises/Chapter2/Exercise1/solution.py
+++ Exercises/Chapter2/Exercise1/solution.py
@@ -1,14 +1,6 @@
-import random
 from timeit import timeit
 from textwrap import dedent
-from typing import List
 from generic_search import linear_contains, binary_contains
-
-
-random.seed(0)
-
-search_space: List[int] = list(range(1000000))
-numbers: List[int] = [random.randint(0, len(search_space)*2) for _ in range(100)]
 
 
 if __name__ == "__main__":
@@ -16,6 +8,9 @@ if __name__ == "__main__":
         print(f"{contains.__name__}:", timeit(dedent(f"""
         for i in numbers:
             {contains.__name__}(search_space, i)"""), dedent(f"""
-        from __main__ import search_space, numbers
-        from generic_search import {contains.__name__}"""),
+        from random import seed, randint
+        from generic_search import {contains.__name__}
+        search_space = list(range(1000000))
+        seed(0)
+        numbers = [randint(0, len(search_space)*2) for _ in range(100)]"""),
         number=1))

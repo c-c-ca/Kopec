e6b8b64b9f90d24a2ddc55f9507e42ecc279c7ad
Adam Carter <adam.clayton.carter@gmail.com>
2020-12-15 18:17:23 -0500

    Modify solution for Exercise 1 from Chapter 2

--- Exercises/Chapter2/Exercise1/solution.py
+++ Exercises/Chapter2/Exercise1/solution.py
@@ -6,11 +6,10 @@ from generic_search import linear_contains, binary_contains
 if __name__ == "__main__":
     for contains in linear_contains, binary_contains:
         print(f"{contains.__name__}:", timeit(dedent(f"""
-        for i in numbers:
-            {contains.__name__}(search_space, i)"""), dedent(f"""
+        for _ in range(100):
+            {contains.__name__}(search_space, randint(0, 10 ** 7))"""), dedent(f"""
         from random import seed, randint
         from generic_search import {contains.__name__}
-        search_space = list(range(1000000))
         seed(0)
-        numbers = [randint(0, len(search_space)*2) for _ in range(100)]"""),
+        search_space = list(range(10 ** 6))"""),
         number=1))
